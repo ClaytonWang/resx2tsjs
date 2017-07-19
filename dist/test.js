81111780010677b9ff46c22251b28b65cc943a04
clay.wang <lovejunlic@hotmail.com>
2017-07-19 16:28:30 +0800

    change module name

--- dist/test.js
+++ dist/test.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var program = require("./index");
-program.executeResxToTs('app.resources', '/custom/node-resx-to-typescript/Resources', '/custom/node-resx-to-typescript/Resources/_generated');
+program.executeResxToTs('app.resources/', '/custom/node-resx-to-typescript/Resources', '/custom/node-resx-to-typescript/Resources/_generated');
 program.executeResxToJson('/custom/node-resx-to-typescript/Resources', '/custom/node-resx-to-typescript/Resources/_generated/json');
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
 import program = require('./index');
 
-program.executeResxToTs('app.resources', '/custom/node-resx-to-typescript/Resources', '/custom/node-resx-to-typescript/Resources/_generated');
+program.executeResxToTs('app.resources/', '/custom/node-resx-to-typescript/Resources', '/custom/node-resx-to-typescript/Resources/_generated');
 program.executeResxToJson('/custom/node-resx-to-typescript/Resources', '/custom/node-resx-to-typescript/Resources/_generated/json');
